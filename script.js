c250cc06a5da70597b503c5706465a43911a4659
MhRantz <meganrantz@gmail.com>
2020-07-21 17:14:24 -0400

    initial commit :coffee:

---
+++ script.js
@@ -0,0 +1,20 @@
+//Watch for user click on the 3 button's 
+//Determine if user is correct by looking up the answer in that terms object in an array of terms
+//Update score with number of guesses, and number that are correct if needed
+//Serve up next term in the array of objects of 1920's slang or cocktails or both
+
+//NEXT STEP OF EXPANSION
+//display meaning of the slang or recipe of the cocktail or both
+
+//NEXT STEP OF EXPANSION
+//cool animations and nice designs
+
+const beesKnees = {
+    terms = [
+        mazuma = {
+            type: both,
+            definition: 'noun: Cash, Money',
+            recipe: '',
+        }
+    ]
+}
